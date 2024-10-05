48232d5b7e5b44ae815916b49a9b02bf5315d119
DefinetlyNotAI <Nirt_12023@outlook.com>
2024-10-06 00:24:55 +0400

    Added 404
    Also updated files, bug fixes etc

--- Website/js/bootstrap.js
+++ Website/js/bootstrap.js
@@ -65,7 +65,7 @@
 
   function _inheritsLoose(subClass, superClass) {
     subClass.prototype = Object.create(superClass.prototype);
-    subClass.prototype.constructor = subClass;
+
     subClass.__proto__ = superClass;
   }
 
