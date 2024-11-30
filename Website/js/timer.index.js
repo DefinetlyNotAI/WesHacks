382c6c494484f011d2d678524e125aa640f4bd27
DefinetlyNotAI <Nirt_12023@outlook.com>
2024-11-30 15:14:42 +0400

    Updated theme

--- Website/js/timer.index.js
+++ Website/js/timer.index.js
@@ -12,8 +12,7 @@ const countdownTimer = setInterval(() => {
         message = `Theme reveal in ${Math.floor(timeLeft / (1000 * 60 * 60 * 24))} days, ${Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))} hours, ${Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60))} minutes, and ${Math.floor((timeLeft % (1000 * 60)) / 1000)} seconds.`;
     } else if (now < hackathonStartDate) {
         const timeLeft = hackathonStartDate - now;
-        <!-- What are you lookin here for?? We aint leakin the theme lil bro -->
-        message = `The theme is NAHHH! Start Practicing 🤗!`;
+        message = `The theme is Astronomy! Start Practicing 🤗!`;
         extraMessage = `Competition in ${Math.floor(timeLeft / (1000 * 60 * 60 * 24))} days, ${Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))} hours, ${Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60))} minutes, and ${Math.floor((timeLeft % (1000 * 60)) / 1000)} seconds.<br><br>Very important, DO NOT finish the project! You won't be allowed to enter the competition if you do. You are allowed to finish the project up to 60% - This will be decided by the organisers.`;
     } else if (now <= hackathonEndDate) {
         message = "The Official Wesgreen Hackathon has begun!";
